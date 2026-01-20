325671281f48cda86988313d2a089e40ea1f8f92
JohanNP-32 <johan.nunez2002@gmail.com>
2026-01-20 15:11:24 -0600

    agrega ejercicio calculadora

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+//alert("Hola Mundo");
+
+function mostrar(){
+    let numero1 = parseInt(document.getElementById("numero1").value);
+    let numero2 = parseInt(document.getElementById("numero2").value);
+    
+    let resultado = 0;
+    resultado = numero1 + numero2;
+
+    let objResultado = document.getElementById("resultado");
+    objResultado.innerHTML = "El resultado de la suma es: " + resultado;
+
+    //alert("El resultado de la suma es: " + resultado);
+}
